2170b852bccbc651ab7932a87229a075978d6276
Brian Olpin <brian.olpin@gettyimages.com>
2021-12-18 16:38:08 -0800

    added email address

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,9 @@ export default function Home() {
         I&apos;ve helped companies large and small with the design and construction of fast, powerful web and database apps that are optimized for search engines and that can grow with your business.</p>
         <p>My technological interests tend towards platforms like Next.js and Rails that find a nice balance between performance, maintainability and flexibility.  Clean code matters! I&apos;ve found that businesses can realize a significant return on investment with the right technology stack and team.</p>
         <p>You won&apos;t get an invoice from me that does not represent <span>significant value</span> delivered.  If you somehow do, simply let me know and <span>don&apos;t pay the invoice</span>.  I&apos;ll gladly refer you to another consultancy that may be a better fit for your needs.</p>
+        <h2>Contact</h2>
+        <p>You can reach me at <b>bolpin@gmail dot com</b>.</p>
+        <p>I live in Seattle, Washington.</p>
       </div>
     </>
   )
